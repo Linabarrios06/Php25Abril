da4e3351f1976c7ade32d1c96d3ef79f93c0a0dc
Meli-Lopez <129072393+Linabarrios06@users.noreply.github.com>
2024-05-16 11:32:50 -0500

    listo

---
+++ 01-Hola-Mundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduccion a PHP - SENA</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contenido">
+        <h1>Desarrollo web completo</h1>
+        <a href="http://www.sena.edu.co" target="_blank">Servicio Nacional de Aprendizaje</a>
+        <p>Introduccion a PHP</p>
+        <p class="hashtag">#Desarrollo Web con PHP</p>
+
+        <div class="resultado">
+
+        </div>
+    </div>
+</body>
+</html>
